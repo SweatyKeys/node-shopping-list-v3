872a2356892c7a953640bf8bf515860a7dba868a
Kevin_King <vegesaurus3@gmail.com>
2018-08-06 12:48:51 -0700

    added delete endpoitn

--- server.js
+++ server.js
@@ -53,6 +53,14 @@ app.delete('/shopping-list/:id', (req, res) => {
   res.status(204).end();
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+})
+
+
+
 
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
@@ -76,6 +84,8 @@ app.post('/recipes', jsonParser, (req, res) => {
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
